5176bd1dd73800da02aa523a4f0a0afdb24791ad
HeyanZhuo <76900053+goxtopia@users.noreply.github.com>
2025-12-14 00:27:12 +0800

    update locale

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,5 +1,12 @@
 pluginManagement {
     repositories {
+
+        // --- 添加下面这块 ---
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        // ------------------
+
         google()
         mavenCentral()
         gradlePluginPortal()
